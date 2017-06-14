a05d05e93fd7094868f942e7dbddce818f42036c
Rogerio Guimaraes <rogerio.guimaraes@mobilecodelabs.com>
2017-06-14 17:08:03 -0300

    done

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://koridallos:kori.1545@ds125262.mlab.com:25262/mcldb',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'mobilecode',
+  masterKey: process.env.MASTER_KEY || 'master_mobilecode', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://mobilecode.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+    classNames: ["GameScore", "Comments"] // List of classes to support for query subscriptions
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
