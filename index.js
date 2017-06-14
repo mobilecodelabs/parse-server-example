094c42472b672826d5395cd4d23a8a8db34ab60e
Rogerio Guimaraes <rogerio.guimaraes@mobilecodelabs.com>
2017-06-14 18:29:31 -0300

    done

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'mobilecode',
   masterKey: process.env.MASTER_KEY || 'master_mobilecode', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://mobilecode.herokuapp.com/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://mobilecode.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["GameScore", "Comments"] // List of classes to support for query subscriptions
   }
